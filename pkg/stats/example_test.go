aec627e136d1bddca4bde49a10b8940f69a276cc
MSHE97 <sherzod.mustafokulov@mail.ru>
2021-02-22 17:46:09 +0500

    добавлены автотесты

---
+++ pkg/stats/example_test.go
@@ -0,0 +1,63 @@
+package stats
+
+import (
+	"fmt"
+
+	"github.com/MSHE97/bank/pkg/types"
+)
+
+func ExampleTotalInCategory()  {
+	payments := []types.Payment{
+		{
+			ID: 1,
+			Amount: 100_00,
+			Category: "cafe",
+		},
+		{
+			ID: 2,
+			Amount: 50_00,
+			Category: "market",
+		},{
+			ID: 3,
+			Amount: 125_00,
+			Category: "transport",
+		},
+		{
+			ID: 4,
+			Amount: 25_00,
+			Category: "cafe",
+		},
+	}
+
+	fmt.Println( TotalInCategory(payments, "cafe") )
+
+	// Output: 12500
+}
+func ExampleAvg() {
+
+	payments := []types.Payment{
+		{
+			ID: 1,
+			Amount: 100_00,
+			Category: "cafe",
+		},
+		{
+			ID: 2,
+			Amount: 50_00,
+			Category: "market",
+		},{
+			ID: 3,
+			Amount: 125_00,
+			Category: "transport",
+		},
+		{
+			ID: 4,
+			Amount: 25_00,
+			Category: "cafe",
+		},
+	}
+
+	fmt.Println( Avg(payments) )
+
+	// Output: 7500
+}
