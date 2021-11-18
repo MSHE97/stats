98ce2c9ab09826be253e91deccfab4128dd76728
MSHE97 <sherzod.mustafokulov@mail.ru>
2021-11-18 17:10:06 +0500

    Updating master

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -2,33 +2,34 @@ package stats
 
 import (
 	"fmt"
+
 	"github.com/MSHE97/bank/v2/pkg/types"
 )
 
-func ExampleTotalInCategory()  {
+func ExampleTotalInCategory() {
 	payments := []types.Payment{
 		{
-			ID: 1,
-			Amount: 100_00,
+			ID:       1,
+			Amount:   100_00,
 			Category: "cafe",
 		},
 		{
-			ID: 2,
-			Amount: 50_00,
+			ID:       2,
+			Amount:   50_00,
 			Category: "market",
-		},{
-			ID: 3,
-			Amount: 125_00,
+		}, {
+			ID:       3,
+			Amount:   125_00,
 			Category: "transport",
 		},
 		{
-			ID: 4,
-			Amount: 25_00,
+			ID:       4,
+			Amount:   25_00,
 			Category: "cafe",
 		},
 	}
 
-	fmt.Println( TotalInCategory(payments, "cafe") )
+	fmt.Println(TotalInCategory(payments, "cafe"))
 
 	// Output: 12500
 }
@@ -36,27 +37,27 @@ func ExampleAvg() {
 
 	payments := []types.Payment{
 		{
-			ID: 1,
-			Amount: 100_00,
+			ID:       1,
+			Amount:   100_00,
 			Category: "cafe",
 		},
 		{
-			ID: 2,
-			Amount: 50_00,
+			ID:       2,
+			Amount:   50_00,
 			Category: "market",
-		},{
-			ID: 3,
-			Amount: 125_00,
+		}, {
+			ID:       3,
+			Amount:   125_00,
 			Category: "transport",
 		},
 		{
-			ID: 4,
-			Amount: 25_00,
+			ID:       4,
+			Amount:   25_00,
 			Category: "cafe",
 		},
 	}
 
-	fmt.Println( Avg(payments) )
+	fmt.Println(Avg(payments))
 
 	// Output: 7500
 }
